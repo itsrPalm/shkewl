4d24afd9991bbb70dada167f0864ab2bcc6617b7
itsrPalm <its.rpalm@gmail.com>
2024-10-22 13:19:19 -0700

    advanced practice push

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "ucarecdn.com",
-            },
-            {
-                protocol: "https",
-                hostname: "advanced-practice.vercel.app",
-            },
-            {
-                protocol: "https",
-                hostname: "img.freepik.com",
-            },
-        ],
-    },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ucarecdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "advanced-practice.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
